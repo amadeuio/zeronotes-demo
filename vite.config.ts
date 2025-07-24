dc2335d554e2f5c7bb3daf11f60c15eff7b9a4ae
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-24 15:10:21 +0200

    chore: update TypeScript configuration and Vite setup with src import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 });
