2578b32d83edbe125e31c8820f6d54598915c2a3
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-24 15:57:09 +0200

    feat: implement basic notes display in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Note } from '@/components';
+import { notes } from '@/data';
+
 const App = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-red-500">
-      <h1>Hello World</h1>
+    <div className="flex h-screen flex-wrap gap-4 bg-zinc-900 p-4 leading-7 text-white">
+      {notes.map((note) => (
+        <Note key={note.id} note={note} />
+      ))}
     </div>
   );
 };
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,7 +5,12 @@ interface NoteProps {
 }
 
 const Note = ({ note }: NoteProps) => {
-  return <></>;
+  return (
+    <div className="flex w-64 flex-col gap-4 rounded-lg border border-zinc-600 p-6 shadow-lg">
+      <h2 className="text-lg">{note.title}</h2>
+      <p className="text-sm">{note.content}</p>
+    </div>
+  );
 };
 
 export default Note;
