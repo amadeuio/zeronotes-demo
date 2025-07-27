b70ba794dbd455f4f8f47b0e1132db5cddc88ea5
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-27 15:24:18 +0200

    feat: add shadcn/ui with button and card components

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
