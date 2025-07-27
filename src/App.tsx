68cafd8be91db301150012775b4ab6cae5271d59
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-27 19:54:44 +0200

    feat: apply dnd to Notes and App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import { Note } from '@/components';
-import { notes } from '@/data';
+import { notes as initialNotes } from '@/data';
+import { DndContext, type DragEndEvent } from '@dnd-kit/core';
+import { SortableContext, arrayMove } from '@dnd-kit/sortable';
+import { useState } from 'react';
 
 const App = () => {
+  const [notes, setNotes] = useState(initialNotes);
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setNotes((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over?.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
-      {notes.map((note) => (
-        <Note key={note.id} note={note} />
-      ))}
+      <DndContext onDragEnd={handleDragEnd}>
+        <SortableContext items={notes.map((note) => note.id)}>
+          {notes.map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
+        </SortableContext>
+      </DndContext>
     </div>
   );
 };
