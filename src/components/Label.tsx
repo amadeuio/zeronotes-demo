f615e55c10490c1213882e5818a0e65e4b30cce4
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-30 09:54:22 +0200

    feat: enhance Label component with icon and text trucation on hover

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,9 +1,18 @@
+import Icon from '@/components/Icon';
+
 interface LabelProps {
   label: string;
 }
 
 const Label = ({ label }: LabelProps) => (
-  <div className="flex gap-2 rounded-full border px-2.5 py-0.5 text-[11px]">{label}</div>
+  <div className="group relative min-w-12 rounded-full border px-2.5 py-0.5 text-[11px]">
+    <div className="truncate group-hover:w-4/5">{label}</div>
+    <Icon
+      name="close"
+      size={11}
+      className="absolute top-[7px] right-1.5 opacity-0 transition-opacity group-hover:opacity-100"
+    />
+  </div>
 );
 
 export default Label;
