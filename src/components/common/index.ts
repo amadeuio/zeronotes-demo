6db4c6130eb4ed66c0426614b871247fd7d8e534
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-10-27 13:06:23 -0600

    chore: remove unused dnd-kit dependencies and components

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,3 @@
-export { default as Drag } from './Drag';
-export { default as Drop } from './Drop';
 export { default as Icon } from './Icon';
 export { default as IconButton } from './IconButton';
 export { default as Menu } from './Menu';
